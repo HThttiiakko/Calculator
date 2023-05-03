e1c0ea532b3cc637ac25c2dac2d6cf99fcbdbb6d
HThttiiakko <132416445+HThttiiakko@users.noreply.github.com>
2023-05-03 11:59:15 +0800

    实现了可交互的按钮

--- Calculator/widget.cpp
+++ Calculator/widget.cpp
@@ -4,10 +4,18 @@
 
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
+    : QWidget(parent), ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    this->setMaximumSize(313, 429);
+    this->setMinimumSize(313, 429);
+
+    this->setWindowTitle("简单计算器");
+
+    QFont f("微软雅黑", 14);    //更改字体和大小
+        ui->lineEdit->setFont(f);
+
 }
 
 Widget::~Widget()
@@ -16,3 +24,134 @@ Widget::~Widget()
 }
 
 
+
+void Widget::on_pushButton1_clicked()
+{
+    exp += "1";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton2_clicked()
+{
+    exp += "2";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton9_clicked()
+{
+    exp += "9";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton3_clicked()
+{
+    exp += "3";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton4_clicked()
+{
+    exp += "4";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton5_clicked()
+{
+    exp += "5";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton6_clicked()
+{
+    exp += "6";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton7_clicked()
+{
+    exp += "7";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton8_clicked()
+{
+    exp += "8";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButton0_clicked()
+{
+    exp += "0";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonzuokuo_clicked()
+{
+    exp += "(";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonyoukuo_clicked()
+{
+    exp += ")";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonchu_clicked()
+{
+    exp += "/";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonmul_clicked()
+{
+    exp += "*";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonpuls_clicked()
+{
+    exp += "+";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonjian_clicked()
+{
+    exp += "-";
+    ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonclear_clicked()
+{
+    exp.clear();
+    ui->lineEdit->clear();
+}
+
+
+void Widget::on_pushButtondel_clicked()
+{
+        exp.chop(1);
+        ui->lineEdit->setText(exp);
+}
+
+
+void Widget::on_pushButtonequal_clicked()
+{
+
+}
--- Calculator/widget.h
+++ Calculator/widget.h
@@ -3,7 +3,8 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QStack>
+#include <string.h>
 
 
 QT_BEGIN_NAMESPACE
@@ -19,8 +20,51 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_pushButton1_clicked();
+
+    void on_pushButton2_clicked();
+
+    void on_pushButton9_clicked();
+
+    void on_pushButton3_clicked();
+
+    void on_pushButton4_clicked();
+
+    void on_pushButton5_clicked();
+
+    void on_pushButton6_clicked();
+
+    void on_pushButton7_clicked();
+
+    void on_pushButton8_clicked();
+
+    void on_pushButton0_clicked();
+
+    void on_pushButtonzuokuo_clicked();
+
+    void on_pushButtonyoukuo_clicked();
+
+    void on_pushButtonchu_clicked();
+
+    void on_pushButtonmul_clicked();
+
+    void on_pushButtonpuls_clicked();
+
+    void on_pushButtonjian_clicked();
+
+    void on_pushButtonclear_clicked();
+
+    void on_pushButtondel_clicked();
+
+
+    void on_pushButtonequal_clicked();
+
 private:
     Ui::Widget *ui;
+    QString exp;
+
+    int Priority(char ch);
 };
 
 #endif // WIDGET_H
